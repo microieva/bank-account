94ecabe3047068fa11cede8d2270396645e390ec
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-02-11 16:14:47 +0200

    saving progress

--- app.js
+++ app.js
@@ -4,80 +4,146 @@ const personAccount = {
     lastName: 'Vyliaudaite',
     incomes: [
         {
-            amount: 0,
             description: "", 
-            date: new Date()   
+            amount: 0,
+            date: ""   
         }
     ],
     expences: [
         {
+            description: "", 
             amount: 0,
-            description: "",
-            date: new Date()
+            date: ""
         }
     ],
-    
-    accountInfo: function() {
-        let report = `\nAccount holder: ${personAccount.firstName.toUpperCase()} ${personAccount.lastName.toUpperCase()}.
-                        \nAccount balance: ${personAccount.accountBalance()}
-                        \nTotal income: ${personAccount.totalIncome()} 
-                        \nTotal expences: ${personAccount.totalExpense()}`
 
-        return report;
+    date: function() {
+        //turn date to seperate strings > into array
+        //loop thru array
+        // take out whats needed
+        const date = new Date();
+        let str =  date.toDateString();
+        let month = '';
+        let day = '';
+        let year = '';
+        //let time 
+        
+        month += (str[4] + str[5] + str[6]);
+        day += (str[8] + str[9]);
+        year += (str[11] + str[12] + str[13] + str[14]);
+        //time += (str[15] + str[16] + str[17] + str[18]);
+        
+        return month+' '+day+', '+year;
     },
 
     //=================================== INCOME ====================================
-    addIncome: function(amount, description) {
-        this.incomes.push({amount, description});
+    addIncome: function() {
+        let userDescr = description.value;
+        let userAmnt = Number(amount.value);
+        const date = this.date();
+        this.incomes.push({userDescr, userAmnt, date});
+        //console.log("addIncome testing")
     },
 
     totalIncome: function() {
-        // let totalIncome = 0;
-        // for (const amount in this.incomes) {
-        //     totalIncome = totalIncome + amount;
-        // }
-        // return totalIncome;
-        return this.incomes.reduce((acc, curr) => {acc + curr.amount}, 0)
+        const total = this.incomes.reduce((acc, curr) => 
+        { 
+            return (acc + Number(curr.amount))
+        },0)
+        //console.log("total income: ", total)
+        return total;   
     },
     
     //=================================== EXPENCES ====================================
-    addExpense: function(amount, description) {
-        this.expences.push({amount, description});
+    addExpence: function() {
+        let userDescr = description.value;
+        let userAmnt = Number(amount.value);
+        const date = this.date();
+        this.expences.push({userDescr, userAmnt, date});
     },
 
-    totalExpense: function() {
-        // let totalExpense = 0;
-        // for (const amount in this.expences) {
-        //     totalExpense = totalExpense + amount;
-        // }
-        // return totalExpense;
-        return this.expences.reduce((acc, curr) => {acc + curr.amount}, 0)
+    totalExpence: function() {
+        const total = this.expences.reduce((acc, curr) => 
+        { 
+            return (acc + Number(curr.amount))
+        },0)
+        console.log("total expence: ", total)
+        return total;
     },
 
     //=================================== BALANCE ====================================
     accountBalance: function() {
-       return this.totalIncome()-this.totalExpense();
+       return this.totalIncome()-this.totalExpence();
     },
 }
 
-console.log(personAccount);
+const buttonAdd = document.querySelector('.button-add');
+let description = document.querySelector('.user-input-description');
+let amount = document.querySelector('.user-input-amount');
+const selectType = document.querySelector('.select');
+const incomeEntry = document.querySelector('.entry-incomes');
+const expenceEntry = document.querySelector('.entry-expences');
+const info = document.querySelector('.balance-info');
 
-//------------------------------------- user -----------------------------------------
-
-//let type = prompt('Enter type: INCOME or EXPENCE');
-//let amount = prompt('Enter amount');
-//let description = prompt('Enter 1-word description')
-
-if (type.toLowerCase() === 'income') {
-    this.addIncome(Number(amount), description.toLowerCase()); //converting user input from string to number  
-
-} else if (type.toLowerCase() === 'expence') { 
-    this.addExpence(Number(amount), description.toLowerCase()); 
-
-} else {
-    prompt('Please mind your spelling!');
+const appendBalance = () => {
+    const balance = `<div>You have ${personAccount.accountBalance()} £ on your account.</div>`
+    info.innerHTML += balance;
 }
 
+const appendIncomeDescription = () => {
+    const userDescription = `<div>${description.value}</div>`;
+    const incomeDescription = document.querySelector('.income-description');
+    incomeDescription.innerHTML += userDescription;
+}
 
-//-----------------------------------------------------------------------------------------------------------
+const appendIncomeAmount = () => {
+    const userAmount = `<div>${amount.value}</div>`
+    const incomeAmount = document.querySelector('.income-amount');
+    incomeAmount.innerHTML += userAmount;
+}
 
+const appendExpenceDescription = () => {
+    const userDescription = `<div>${description.value}</div>`;
+    const expenceDescription = document.querySelector('.expence-description');
+    expenceDescription.innerHTML += userDescription;
+}
+
+const appendExpenceAmount = () => {
+    const userAmount = `<div>${amount.value}</div>`
+    const expenceAmount = document.querySelector('.expence-amount');
+    expenceAmount.innerHTML += userAmount;
+}
+
+const appendIncomeDate = () => {
+    const incomeDate = document.querySelector('.income-date');
+    incomeDate.innerHTML += `<div>${personAccount.date()}</div>`
+}
+
+const appendExpenceDate = () => {
+    const expenceDate = document.querySelector('.expence-date');
+    expenceDate.innerHTML += `<div>${personAccount.date()}</div>`
+}
+
+buttonAdd.addEventListener('click', () => {
+    if (selectType.value === 'INCOME' && description.value != '' && amount.value != '') {
+        personAccount.addIncome();
+        appendIncomeDescription();
+        appendIncomeAmount();
+        appendIncomeDate()
+        console.log("CHECKING CONDITION")
+    } 
+    if (selectType.value === 'EXPENCE'&& description.value != '' && amount.value != '') {
+        personAccount.addExpence();
+        appendExpenceDescription();
+        appendExpenceAmount();
+        appendExpenceDate();
+
+    }
+    appendBalance();
+    amount.value = '';
+    description.value = '';
+})
+console.log("calling personAccount.accountBalance(): ", personAccount.accountBalance())
+console.log("type of accountBalance(): ", typeof personAccount.accountBalance())
+console.log("type of totalIncome(): ", typeof personAccount.totalIncome())
+console.log("type of totalExpence(): ",typeof personAccount.totalExpence())
